3cdfb0d4732c67a25fb86a1d5e6fee00713e7707
alextavella <alex.tavella-ext@viavarejo.com.br>
2020-06-19 11:30:36 -0300

    Implementation API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRespositories] = useState([]);
+
+  useEffect(() => {
+    try {
+      api.get('/repositories').then(response => {
+        setRespositories(response.data);
+      });
+    } catch (e) {
+      console.log('Erro ao carregar repositorios', e.message);
+    }
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const repository = {
+        "url": "https://github.com/alextavella",
+        "title": `Novo Repository ${Date.now()}`,
+        "techs": ["React", "ReactJS", "ReactNative", "TypeScript"],
+      };
+
+      const response = await api.post('/repositories', repository);
+
+      setRespositories([...repositories, response.data]);
+    } catch (e) {
+      console.log('Erro ao cadastrar repositorio', e.message);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      setRespositories(repositories.filter(r => r.id !== id));
+    } catch (e) {
+      console.log('Erro ao remover repositorio', e.message);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
